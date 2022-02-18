79abc567afc9c91949815aab2252c980e21e4e17
Иван <vanya-komarov-04k@yandex.ru>
2022-02-18 19:54:23 +0300

    Problema

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,7 +1,5 @@
 def welcome_user():
-
     import prompt
     name = prompt.string('May I have your name? ')
-
-    if name != '':
-        print('Hello, ' + name + '!')
+    print('Hello, ' + name + '!')
+    return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from brain_games.cli import welcome_user
-
-
+from brain_gamse.scripts.brain_even import game
 def main():
+
     print("Welcome to the Brain Games!")
     welcome_user()
-
+    game()
 
 if __name__ == '__main__':
     main()
